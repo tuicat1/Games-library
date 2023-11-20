7a19bbf1d81c1474a182f492892987fa0b30bfef
Austen Edge <austen.edge@xtra.co.nz>
2023-11-20 15:52:44 +1300

    button colours

--- index.js
+++ index.js
@@ -127,7 +127,7 @@ function initializeGauge(value, containerId) {
   }
   
  // Function to create status buttons
-function createStatusButton(status) {
+ function createStatusButton(status) {
     const button = document.createElement("button");
     button.classList.add("index-status-button");
     button.id = `${status.toLowerCase()}-button`;
@@ -138,7 +138,7 @@ function createStatusButton(status) {
             button.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24" width="30" height="30"><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm-1-11h2v4h-2zm0 5h2v2h-2z"/></svg>`;
             break;
         case "Playing":
-            button.innerHTML = `<svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="gamepad" class="svg-inline--fa fa-gamepad fa-2xl" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512" color="inherit"><path fill="currentColor" d="M192 64C86 64 0 150 0 256S86 448 192 448H448c106 0 192-86 192-192s-86-192-192-192H192zM496 248c-22.1 0-40-17.9-40-40s17.9-40 40-40s40 17.9 40 40s-17.9 40-40 40zm-24 56c0 22.1-17.9 40-40 40s-40-17.9-40-40s17.9-40 40-40s40 17.9 40 40zm-168-88c0-13.3 10.7-24 24-24s24 10.7 24 24v32h32c13.3 0 24 10.7 24 24s-10.7 24-24 24H344v32c0 13.3-10.7 24-24 24s-24-10.7-24-24v-32H232c-13.3 0-24-10.7-24-24s10.7-24 24-24h32v-32z"></path></svg>`;
+            button.innerHTML = `<svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="gamepad" class="svg-inline--fa fa-gamepad fa-2xl" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512" color="inherit"><path fill="currentColor" d="M192 64C86 64 0 150 0 256S86 448 192 448H448c106 0 192-86 192-192s-86-192-192-192H192zM496 248c-22.1 0-40-17.9-40-40s17.9-40 40-40s40 17.9 40 40s-17.9 40-40 40zm-24 56c0 22.1-17.9 40-40 40s-40-17.9-40-40s17.9-40 40-40s40 17.9 40 40zM168 200c0-13.3 10.7-24 24-24s24 10.7 24 24v32h32c13.3 0 24 10.7 24 24s-10.7 24-24 24H216v32c0 13.3-10.7 24-24 24s-24-10.7-24-24V280H136c-13.3 0-24-10.7-24-24s10.7-24 24-24h32V200z"></path></svg>`;
             break;
         case "Played":
             button.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24" width="30" height="30"><path d="M9 16.17L4.83 12l-1.42 1.41L9 19 21 7l-1.41-1.41L9 16.17z"/></svg>`;
@@ -146,6 +146,19 @@ function createStatusButton(status) {
         default:
             break;
     }
+    button.addEventListener("click", function () {
+        // Toggle button color on press
+        const currentColor = button.style.backgroundColor;
+    
+        if (currentColor === "green") {
+            // If the button is already the desired color, remove the color
+            button.style.removeProperty("background-color");
+        } else {
+            // If the button is not the desired color, set the desired color
+            button.style.backgroundColor = "green";
+        }
+    });
+
 
     return button;
 }
